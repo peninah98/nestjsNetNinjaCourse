b7c94a5c3b2e4a8ab8ee588740fec51748d2cd13
peninah98 <peninangizwenayo@gmail.com>
2023-12-08 12:21:34 +0200

    feat: creating the user end point

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  @Get()
+  getUser() {
+    return [];
+  }
+  @Get(':id')
+  getUserById() {
+    return {};
+  }
+}
