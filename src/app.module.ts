e20d15fa54edfbb03d31895eba1a46cf8b999609
peninah98 <peninangizwenayo@gmail.com>
2023-12-08 12:07:25 +0200

    feat: genetating the resources using the commands

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
