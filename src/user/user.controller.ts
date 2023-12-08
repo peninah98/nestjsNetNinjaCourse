464615a19de53730e331404f3abb4a8f708e5830
peninah98 <peninangizwenayo@gmail.com>
2023-12-08 12:58:03 +0200

    feat: creating put and delete functionalities

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,43 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
 
 @Controller('user')
 export class UserController {
   @Get()
-  getUser() {
-    return [];
+  getUser(@Query('type') type: string) {
+    return [{ type }];
   }
   @Get(':id')
-  getUserById() {
+  getUserById(@Param('id') id: string) {
+    return {
+      id,
+    };
+  }
+
+  @Post()
+  createUser() {
+    return {};
+  }
+
+  @Delete(':id')
+  deleteUser() {
+    return {};
+  }
+
+  @Put(':id')
+  updateUser() {
+    return {};
+  }
+
+  @Delete()
+  deleteAllUsers() {
     return {};
   }
 }
